69234a3d00e335c110908c8377c0c885442f63e8
GGolfz <wisarutgolf11@gmail.com>
2021-12-06 20:49:16 +0700

    add: sandbox :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { Sandpack, getThemeStyleSheet } from "@codesandbox/sandpack-react";
+import "@codesandbox/sandpack-react/dist/index.css";
 
 function App() {
+  const [template, setTemplate] = useState("react");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{fontSize:'2.5rem'}}>GGolfz's Sandbox</h1>
+        <div style={{display:'flex', fontSize:'1.25rem'}}>
+          <div style={{padding:'1rem',margin:'.25rem'}} onClick={() => setTemplate("vanilla")}>Vanila</div>
+          <div style={{padding:'1rem',margin:'.25rem'}} onClick={() => setTemplate("react")}>React</div>
+          <div style={{padding:'1rem',margin:'.25rem'}} onClick={() => setTemplate("react-ts")}>React TypeScript</div>
+          <div style={{padding:'1rem',margin:'.25rem'}} onClick={() => setTemplate("vue")}>VueJS</div>
+          <div style={{padding:'1rem',margin:'.25rem'}} onClick={() => setTemplate("vue3")}>Vue3</div>
+        </div>
+        <div style={{ width: "100vw" }}>
+          <Sandpack
+            template={template}
+            theme="sandpack-dark"
+            options= {{
+              editorHeight: "80vh",
+              showInlineErrors: true,
+              showNavigator: true,
+              showEditor: true,
+              showLineNumbers: true,
+              showTabs: true,
+            }}
+          />
+        </div>
       </header>
     </div>
   );
